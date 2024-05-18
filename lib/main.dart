3c00527d6f78184bc35df1de85e34d48b1617dd0
space <93774811+65535space@users.noreply.github.com>
2024-05-19 00:09:39 +0900

    スクロールできるタスクリストを作ろう

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,82 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'タスク管理アプリ',
-      home: Header(),
+      home: MyAppScaffold(),
     );
   }
 }
 
-class Header extends StatelessWidget {
-  const Header({super.key});
+class MyAppScaffold extends StatelessWidget {
+  const MyAppScaffold({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text("タスク管理アプリ"),
       ),
       body: const Column(
         children: [
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.star),
-                onPressed: null,
-              ),
-              TextButton(
-                  onPressed: null,
-                  child: Row(
-                    children: [
-                      Icon(Icons.add),
-                      Text("新しいリスト"),
-                    ],
-                  )),
-            ],
-          )
+          TaskListButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class TaskListButton extends StatefulWidget {
+  const TaskListButton({super.key});
+
+  @override
+  TaskListButtonState createState() => TaskListButtonState();
+}
+
+class TaskListButtonState extends State<TaskListButton> {
+  List<Widget> buttons = [];
+
+  void addButton() {
+    setState(() {
+      buttons.add(
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0),
+            ),
+            elevation: 0,
+          ),
+          child: Text("${buttons.length + 1}"),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          const IconButton(
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            icon: Icon(Icons.star),
+            iconSize: 25,
+            onPressed: null,
+          ),
+          ...buttons,
+          TextButton(
+            onPressed: () {
+              addButton();
+            },
+            child: const Row(
+              children: [
+                Icon(Icons.add),
+                Text("新しいリスト"),
+              ],
+            ),
+          ),
         ],
       ),
     );
